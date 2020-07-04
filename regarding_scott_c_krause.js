48299ca08bf0d907ceb4b516887058a2f711461f
Scott C. Krause <3151842+neodigm@users.noreply.github.com>
2020-07-04 00:19:24 -0500

    🏖️ Inspired Problem Solver 🚀 Visual Storyteller

--- regarding_scott_c_krause.js
+++ regarding_scott_c_krause.js
@@ -1,28 +1,32 @@
 <script type="application/ld+json">
-    {
-      "@context": "https://schema.org",
-      "@type": "Person",
-      "address": {
-        "@type": "PostalAddress",
-        "addressLocality": "Lake County",
-        "addressRegion": "IL",
-        "postalCode": "60031"
-      },
-      "email": "neo5ive@gmail.com",
-      "image": "http://www.thescottkrause.com/ux/typescript/engineer/wasm/self.png",
-      "jobTitle": "Sr. UX Front-end Engineer",
-      "name": "Scott Krause",
-      "url": "https://www.theScottKrause.com",
-	    "sameAs" : [ "https://github.com/neodigm",
-	    "https://www.theScottKrause.com/neodigm-wasm-golang",
-"https://www.npmjs.com/~neodigm",
+  {
+    "@context": "https://schema.org",
+    "@type": "Person",
+    "address": {
+      "@type": "PostalAddress",
+      "addressLocality": "Lake County",
+      "addressRegion": "IL",
+      "postalCode": "60031"
+    },
+    "email": "neo5ive@gmail.com",
+    "image": "https://neodigm.github.io/brand_logo_graphic_design/fantastic/discerning/24.jpg",
+    "jobTitle": "Sr. UX Front-end Engineer",
+    "name": "Scott Krause",
+    "url": "https://www.theScottKrause.com",
+    "sameAs" : [ "https://github.com/neodigm",
+"https://www.theScottKrause.com/",
+"https://thescottkrause.com/Aardvark_Scott_C_Krause_2020.pdf",
+"https://thescottkrause.com/d3_datavis_skills.html",
 "https://gist.github.com/neodigm",
-	    "https://www.linkedin.com/in/neodigm24/",
-	    "https://trailblazer.me/id/skrause",
-	    "https://codepen.io/neodigm24",
-	    "https://machvive.com/",
-	    "https://www.arcanus55.com/",
-      "https://repl.it/@neodigm",
-      "https://twitter.com/neodigm24"]
-    }
+"https://www.npmjs.com/~neodigm",
+"https://www.linkedin.com/in/neodigm24/",
+"https://trailblazer.me/id/skrause",
+"https://codepen.io/neodigm24",
+"https://machvive.com/",
+"https://www.arcanus55.com/",
+"https://repl.it/@neodigm",
+"https://twitter.com/neodigm24",
+"https://hub.docker.com/u/neodigm",
+"https://www.w3.org/users/123844"]
+  }
 </script>
