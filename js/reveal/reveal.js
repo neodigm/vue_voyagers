4483be63a709aa9a93fbef9fc01827a603b227c2
Scott C. Krause <3151842+neodigm@users.noreply.github.com>
2021-09-12 19:14:55 -0500

    👁️ D3 Parallax Three.js && WebGL && GSAP 🍭

--- js/reveal/reveal.js
+++ js/reveal/reveal.js
@@ -1,4 +1,4 @@
-"use strict"; // A meticulous merger of form and function
+"use strict"; // A meticulous merger of form and function (Vanilla JS Comp)
 var _aRevAct=0, _aRevX=0, _sRevId="", _bIsOpen = false, _fOnClose=null;
 var a55Rev = {
   eRev: 0, eRevScrim: 0,
